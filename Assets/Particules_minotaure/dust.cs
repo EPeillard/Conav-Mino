1c3d872b86fbabd405bbae93f603904f8191670c
RemyMaige <46453539+RemyMaige@users.noreply.github.com>
2019-03-15 10:30:42 +0100

    "update dust 15/03"

--- Assets/Particules_minotaure/dust.cs
+++ Assets/Particules_minotaure/dust.cs
@@ -2,25 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class dust : MonoBehaviour
 {
-    public GameObject poussière;
-    float timer = 0;
-    // Update is called once per frame
-    void Update()
-    {
-        
-        
+    public GameObject poussiere;
+    public GameObject[] murs;
+    public Animator animator;
 
+    private float timer = 0;
+    private float lifetime = 0;
+    private float timerDespawn = 0;
+    private bool isSpawn;
+
+    private int i = 0;
+
+    private GameObject daddy;
+    private GameObject clone;
+    
+    private void Start()
+    {
+        lifetime = poussiere.GetComponent<ParticleSystem>().main.duration + poussiere.GetComponent<ParticleSystem>().startLifetime;
+        murs = GameObject.FindGameObjectsWithTag("wall");
+        while (murs[i].name != "door")
+        {
+            i += 1;
+            if (i == murs.Length)
+            {
+                Debug.LogError("ERREUR : Porte non détectée");
+                break;
+            }
+        }
+    }
+    // Update is called once per frame
+
+    private void Update()
+    {
+
+        timer += Time.deltaTime;
+        
         if (timer > 10)
         {
-            Instantiate(poussière);
+            
+            i = i % murs.Length;
+            daddy = murs[i];
+            clone = Instantiate(poussiere);
+            clone.transform.parent = daddy.transform;
+
+            //clone.transform.position += daddy.transform.position;
+
+            clone.transform.localPosition = new Vector3(0, 0, 0.5f);
+            clone.transform.position += new Vector3(0, daddy.GetComponent<Renderer>().bounds.size.y * 0.8f, 0);
+            clone.transform.position += daddy.transform.position - clone.transform.position;
+            clone.transform.eulerAngles = new Vector3(180, daddy.transform.eulerAngles.y, 90);
+
+            //TESTS UNITAIRES MDR
+            Debug.LogError(" différence entre la position voulue et la position de la poussière" + (daddy.transform.position - clone.transform.position));
+            
+
             timer = 0;
+            i++;
+            isSpawn = true;
         }
-        else
+        if (isSpawn && timer >lifetime)
         {
-            timer += Time.deltaTime;
+            Destroy(clone);
         }
-        Debug.Log(timer);
     }
 }
