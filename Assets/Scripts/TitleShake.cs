90912380688b8d1f7f5e259b9afcbdeb3ba281d9
Cubic Otter <gaetan.mf@outlook.fr>
2019-03-22 12:03:34 +0100

    Ajout de l'ecran de demarrage Creation d'un decors de demarrage Ajout d'un texte de titre

---
+++ Assets/Scripts/TitleShake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleShake : MonoBehaviour
+{
+    float speed = 0.5f; //how fast it shakes
+    float amount = 0.5f; //how much it shakes
+    Vector3 startingPos;
+    float x, y, z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos.x = transform.position.x;
+        startingPos.y = transform.position.y;
+        startingPos.z = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = startingPos.x;// + Mathf.Sin(Time.time * speed) * amount;
+        y = startingPos.y; // + Mathf.Sin(Time.time * speed) * amount;
+        z = startingPos.z + Mathf.Sin(Time.time * speed) * amount;
+        transform.position = new Vector3(x,y,z);
+    }
+}
